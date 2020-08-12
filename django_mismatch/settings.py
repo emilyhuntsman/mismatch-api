13d5b59a1a1ecd67a7a35b028e5f8a84ba71914e
Emily Huntsman <emilyhuntsman@MacBook-Pro-8.local>
2020-08-12 13:49:37 -0700

    removing static dir

--- django_mismatch/settings.py
+++ django_mismatch/settings.py
@@ -131,5 +131,5 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (os.path.join(PROJECT_ROOT, 'static')),
+# STATICFILES_DIRS = (os.path.join(PROJECT_ROOT, 'static')),
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
