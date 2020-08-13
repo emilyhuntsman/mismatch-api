e291064889ea9b7f9644ca18b64257e15b8715ad
Emily Huntsman <emilyhuntsman@MacBook-Pro-8.local>
2020-08-13 08:58:46 -0700

    switched to correct serializer

--- mismatch/serializers.py
+++ mismatch/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 from .models import Card
 
-class CardSerializer(serializers.HyperlinkedModelSerializer):
+class CardSerializer(serializers.ModelSerializer):
     class Meta:
         model = Card
         fields = ('q','a','b')
