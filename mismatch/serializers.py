d1a4ed7f30b113cff202ef32384b0b2905c7ab1b
Emily Huntsman <emilyhuntsman@MacBook-Pro-8.local>
2020-08-13 10:12:24 -0700

    adding login view

--- mismatch/serializers.py
+++ mismatch/serializers.py
@@ -1,7 +1,17 @@
 from rest_framework import serializers
 from .models import Card
+from django.contrib.auth.models import User
 
 class CardSerializer(serializers.ModelSerializer):
     class Meta:
         model = Card
         fields = ('q','a','b')
+
+class UserSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = User
+        fields = ('username', 'password', 'email')
+
+class TokenSerializer(serializers.Serializer):
+    token = serializers.CharField(max_length=255)
--- mismatch/views.py
+++ mismatch/views.py
@@ -1,9 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import JsonResponse
-from rest_framework import generics
+from rest_framework import generics, permissions, status
+from rest_framework.response import Response
 from .models import Card
-from .serializers import CardSerializer
+from django.contrib.auth.models import User
+from .serializers import CardSerializer, UserSerializer, TokenSerializer
+from django.contrid.auth import authenticate, login
+from rest_framework_simplejwt.tokens import RefreshToken
 
 class CardList(generics.ListCreateAPIView):
     queryset = Card.objects.all()
     serializer_class = CardSerializer
+
+class LoginView(generics.ListCreateAPIView):
+    """
+    POST user/login/
+    """
+    permission_classes = (permissions.AllowAny,)
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+
+    def post(self, request, *args, **kwargs):
+        username = request.data.get("username", "")
+        password = request.data.get("password", "")
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            # login saves the user’s ID in the session,
+            # using Django’s session framework.
+            login(request, user)
+            refresh = RefreshToken.for_user(user)
+            serializer = TokenSerializer(data={
+                # using DRF JWT utility functions to generate a token
+                "token": str(refresh.access_token)
+                })
+            serializer.is_valid()
+            return Response(serializer.data)
+        return Response(status=status.HTTP_401_UNAUTHORIZED)
