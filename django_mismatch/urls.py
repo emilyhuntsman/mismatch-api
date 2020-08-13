5b7172b95edd1385629411257d55580dbc145b62
Emily Huntsman <emilyhuntsman@MacBook-Pro-8.local>
2020-08-13 09:24:28 -0700

    added cors whitelist and simple jwt

--- django_mismatch/urls.py
+++ django_mismatch/urls.py
@@ -13,14 +13,20 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include
 from mismatch import views
 from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt import views as jwt_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('cards/', views.CardList.as_view(), name='card_list'),
+    path('api/token/', jwt_views.TokenObtainPairView.as_view(),
+         name='token_obtain_pair'),
+    path('api/token/refresh/', jwt_views.TokenRefreshView.as_view(),
+         name='token_refresh'),
 ]
