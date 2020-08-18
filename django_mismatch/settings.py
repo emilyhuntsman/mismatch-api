1ff78f653c45dd7bb24f6272703582b076e11890
Emily Huntsman <emilyhuntsman@MacBook-Pro-8.local>
2020-08-18 13:09:14 -0700

    setting up if/else for database, heroku deployment

--- django_mismatch/settings.py
+++ django_mismatch/settings.py
@@ -89,21 +89,28 @@ WSGI_APPLICATION = 'django_mismatch.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-
-# below for local use of postgres
-DATABASES = {
-    'default': {
-    	'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'mismatch',
-        'USER': 'mismatchuser',
-        'PASSWORD': 'mismatch',
-        'HOST': 'localhost',
-        'PORT': '5432',
+if 'RDS_DB_NAME' in os.environ:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': os.environ['RDS_DB_NAME'],
+            'USER': os.environ['RDS_USERNAME'],
+            'PASSWORD': os.environ['RDS_PASSWORD'],
+            'HOST': os.environ['RDS_HOSTNAME'],
+            'PORT': os.environ['RDS_PORT'],
+        }
     }
-}
-# DATABASES = {
-#     'default' : dj_database_url.config(conn_max_age=600, ssl_require=True)
-# }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'mismatch',
+            'USER': 'mismatchuser',
+            'PASSWORD': 'mismatch',
+            'HOST': 'localhost',
+            'PORT': '5432',
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
