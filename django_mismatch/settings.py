2e78220d37470d455b9139880221221c750d8e2b
Emily Huntsman <emilyhuntsman@MacBook-Pro-8.local>
2020-08-18 13:26:48 -0700

    fixing indent

--- django_mismatch/settings.py
+++ django_mismatch/settings.py
@@ -89,16 +89,16 @@ WSGI_APPLICATION = 'django_mismatch.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql',
-            'NAME': 'mismatch',
-            'USER': 'mismatchuser',
-            'PASSWORD': 'mismatch',
-            'HOST': 'localhost',
-            'PORT': '5432',
-        }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mismatch',
+        'USER': 'mismatchuser',
+        'PASSWORD': 'mismatch',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
